00c0d786232cceb7707e8a64dfd52909404248f3
stevan.matovic <stevan.matovic95@gmail.com>
2021-04-26 20:06:40 +0200

    my 3rd commit

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 class MyClass:
 
     def __init__(self):
-        self.value = 0
+        self.value = 6
         print("Initialized")
     
     def method1(self):
-        self.value += 1
-        print(f"Value is now {sekf.value}")
+        self.value -= 1
+        print(f"Value is now {self.value}")
 
     def method2(self):
-        print("Method 2 called")
+        print("Fucking git")
 
 myObject = MyClass()
 myObject.method1()

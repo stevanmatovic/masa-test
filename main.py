3f2719e818c3139fa1fbe0fdedf24c2974b69589
stevan.matovic <stevan.matovic95@gmail.com>
2021-04-26 19:23:28 +0200

    my-commit

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 class MyClass:
 
     def __init__(self):
+        self.value = 0
         print("Initialized")
     
     def method1(self):
-        print("Method 1 called")
+        self.value += 1
+        print(f"Value is now {sekf.value}")
 
+    def method2(self):
+        print("Method 2 called")
 
 myObject = MyClass()
 myObject.method1()
+myObject.method2()

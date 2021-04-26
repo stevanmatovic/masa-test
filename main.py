a7cbc70e73184cbb46deb58126fbd7164ca4ea6b
stevan.matovic <stevan.matovic95@gmail.com>
2021-04-26 19:16:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+class MyClass:
+
+    def __init__(self):
+        print("Initialized")
+    
+    def method1(self):
+        print("Method 1 called")
+
+
+myObject = MyClass()
+myObject.method1()
